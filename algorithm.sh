03d110b9b0a0a28d6e4af2344b7eaa1dc16b3241
Vicky2817 <99022730+Vicky2817@users.noreply.github.com>
2025-03-15 13:55:39 +0530

    Add files via upload

---
+++ algorithm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ITERATIONS=1000000
+inside_circle=0
+
+for ((i=0; i<ITERATIONS; i++)); do
+    x=$(awk -v seed=$RANDOM 'BEGIN { srand(seed); print rand() }')
+    y=$(awk -v seed=$RANDOM 'BEGIN { srand(seed); print rand() }')
+    
+    distance=$(echo "$x*$x + $y*$y" | bc -l)
+    
+    if (( $(echo "$distance <= 1" | bc -l) )); then
+        ((inside_circle++))
+    fi
+done
+
+pi=$(echo "4 * $inside_circle / $ITERATIONS" | bc -l)
+echo "Estimated value of Pi: $pi"
